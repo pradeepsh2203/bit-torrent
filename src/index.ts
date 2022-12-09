f80a928cf3c8104c373a2bd0dd57c7a916e72667
Pradeep Sharma <pradeepsharma.sharma095@gmail.com>
2022-12-09 17:01:09 +0530

    Added some basic express templates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { Request, Response, Express } from "express";
+import { LiveReloadServer } from "livereload";
 const express = require("express");
+const livereload: LiveReloadServer = require("livereload");
 require("dotenv").config();
 // Configuring our Express App
 const app: Express = express();
+app.set("view engine", "ejs");
 
 // The Landing route
 app.get("/", (req: Request, res: Response) => {
-	res.send("This is my site Landing Page");
+	res.render("template/index.ejs");
 });
 
 // Running our server on a port
 app.listen(process.env.PORT || 5000, () => {
 	console.log("The server is running at port ", process.env.PORT || 5000);
 });
+livereload.watch(__dirname + "../views");
